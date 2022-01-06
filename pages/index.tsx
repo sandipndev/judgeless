6d5fc5094dd75caa577794cc591447a0a7848ed0
Trisha Chatterjee <trisha.chatterjee2000@gmail.com>
2022-01-06 13:10:14 +0530

    added slick slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,6 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import type { NextPage } from 'next'
 import Image from 'next/image'
 import { withAuthUser, AuthAction, withAuthUserTokenSSR } from 'next-firebase-auth'
@@ -15,10 +18,41 @@ const Home: NextPage = () => {
     await firebase.auth().signInWithRedirect(provider)
   };
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  };
+
   return (
     <header className="w-screen flex flex-col justify-center items-center space-y-4">
       <Image src={Logo} alt="Logo" />
-      <Quote body="Don't be afraid. Be focused. Be determined. Be hopeful. Be empowered." by="Michelle Obama" title="Focus" />
+      <div className="text-white inline-block">
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3 className="text-white">1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
       <button className="btn" onClick={login}>
         Login/Register with Google
       </button>
