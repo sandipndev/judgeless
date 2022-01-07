6e2ab7938a96b3101c90b57236722d9593276bb8
Sandipan Dey <hey@sandipan.dev>
2022-01-07 18:06:27 +0530

    fix: slick carousel shows up and autoplays

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -8,7 +8,7 @@ interface QuoteProps {
 
 const Quote: NextPage<QuoteProps> = ({ body, by, title }) => (
   <div className="w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800" style={{ maxWidth: "500px" }}>
-    <div className="w-full mb-10">
+    <div className="w-full mb-6">
       <div className="text-3xl text-indigo-500 text-left leading-tight h-3">“</div>
       <p className="text-xl text-gray-600 text-center px-5">{body}</p>
       <div className="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">”</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 import type { NextPage } from 'next'
 import Image from 'next/image'
 import { withAuthUser, AuthAction, withAuthUserTokenSSR } from 'next-firebase-auth'
@@ -12,6 +10,9 @@ import Quote from '../components/Quote'
 import FullPageLoader from '../components/FullPageLoader'
 import Logo from "../public/logo-animated.gif"
 
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
 const Home: NextPage = () => {
   const login = async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -19,38 +20,23 @@ const Home: NextPage = () => {
   };
 
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true
+    autoplay: true,
+    autoPlaySpeed: 1000,
   };
 
   return (
     <header className="w-screen flex flex-col justify-center items-center space-y-4">
       <Image src={Logo} alt="Logo" />
-      <div className="text-white inline-block">
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <h3 className="text-white">1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+      <div className="text-white w-5/6 sm:w-1/2">
+        <Slider className="text-white w-full" {...settings}>
+          <Quote body="Don't be afraid. Be focused. Be determined. Be hopeful. Be empowered." by="Michelle Obama" title="Focus" />
+          <Quote body="We should not give up and we should not allow the problem to defeat us." by="A. P. J. Abdul Kalam" title="Motivation" />
+          <Quote body="A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing." by="John Keats" title="Mistakes" />
         </Slider>
       </div>
       <button className="btn" onClick={login}>
